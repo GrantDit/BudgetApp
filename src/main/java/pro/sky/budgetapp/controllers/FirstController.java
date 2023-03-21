332ad18ebcce17eef89fad5bac6f74e0f8cbe583
GrantDit <kiruha5854@mail.ru>
2023-03-21 16:53:13 +0300

    HomeWork ready

--- src/main/java/pro/sky/budgetapp/controllers/FirstController.java
+++ src/main/java/pro/sky/budgetapp/controllers/FirstController.java
@@ -1,4 +1,20 @@
 package pro.sky.budgetapp.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.awt.SystemColor.info;
+
+@RestController
 public class FirstController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String page() {
+        return "Имя ученика: Кирилл ," + " Название проекта: BudgetApp ," + " Дата создани: 21.03.2023 ," + " Описание: Приложение позволит построить бюджет.";
+    }
 }
